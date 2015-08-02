0252731295cecc6769fcf70ed57b0dcdc90fd9c3
codeRepJamie <jamiextsang@gmail.com>
2015-08-03 01:45:50 +0800

    check

--- js/app.js
+++ js/app.js
@@ -1,45 +1,43 @@
-var app=angular.module('app',[
+var app = angular.module('app', [
     'ngAnimate',
     'ngRoute',
     'appController'
 ]);
-app.config(['$routeProvider',function($routeProvider){
-    $routeProvider.when('/abc',{
-        templateUrl:'temp/abc.html'
-    }).when('/abc/def',{
-        templateUrl:'temp/def.html'
-    }).when('/ABC',{
-        template:'<h1 class="ABC">ABC</h1>'
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/abc', {
+        templateUrl: 'temp/abc.html'
+    }).when('/blog', {
+        template: '<iframe src="/blog/" frameborder="0"></iframe>'
+    }).when('/ABC', {
+        template: '<h1 class="ABC">ABC</h1>'
     }).otherwise({
-        redirectTo:'/abc'
+        redirectTo: '/abc'
     });
 }]);
 
-var appController=angular.module('appController',[]);
-appController.controller('viewCtrl',['$rootScope','$scope','$location',function($rootScope,$scope,$location){
-    $scope.$on('$routeChangeSuccess',function(event){
-        $scope.isBack=false;
-        angular.forEach($rootScope.temp,function(obj,index){
-            if(/^\/\w+\/\w+$/.test($location.path())){
-                $scope.isBack=false;
-                return false;
-            }else{
-                $scope.isBack=true;
-            }
-        });
+var appController = angular.module('appController', []);
+appController.controller('viewCtrl', ['$rootScope', '$scope', '$location', function ($rootScope, $scope, $location) {
+    $scope.$on('$routeChangeSuccess', function (event) {
+        if(/\/blog/.test($location.path())){
+            $scope.isBack = false;
+            return false;
+        }else{
+            $scope.isBack = true;
+        }
     })
 }]);
-app.run(['$rootScope',function($rootScope){
-    $rootScope.temp=[{
-            url: '/abc',
-            children: {
-                url: '/def'
-            }
-        },{
+app.run(['$rootScope', function ($rootScope) {
+    $rootScope.temp = [{
+        url: '/abc'
+    }, {
         url: '/ABC',
         children: {
             url: '/def'
         }
-    }
+    },
+        {
+            url: '/blog'
+        }
+
     ];
 }]);
